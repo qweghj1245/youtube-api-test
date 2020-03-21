6410e7bc1ca95e05495d80abf89db6ea89a7364d
mazusu <qweghj1245@gmail.com>
2020-03-21 22:12:09 +0800

    Updates

---
+++ precache-manifest.90cfccc0340ff37a6b3b33d0a432b71c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665ab49d0dfd3b66c1bb152f79eeea28",
+    "url": "/youtube-api-test/index.html"
+  },
+  {
+    "revision": "2c26ee5bdfd499b9d84e",
+    "url": "/youtube-api-test/static/css/main.bc61e893.chunk.css"
+  },
+  {
+    "revision": "cbb45c6fb2c74ab2e11a",
+    "url": "/youtube-api-test/static/js/2.73b42dab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/youtube-api-test/static/js/2.73b42dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c26ee5bdfd499b9d84e",
+    "url": "/youtube-api-test/static/js/main.a43d845d.chunk.js"
+  },
+  {
+    "revision": "5957feca3adb5bfee1af",
+    "url": "/youtube-api-test/static/js/runtime-main.9d2c0696.js"
+  },
+  {
+    "revision": "cd9e75b5d0be6e074716cd1264777698",
+    "url": "/youtube-api-test/static/media/Icon-search.cd9e75b5.svg"
+  }
+]);
